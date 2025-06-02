795d273ab6ad158eb1e06930b617731c4c0ef8d1
Haifaa-t <h.aloraini-t@tamkeentech.sa>
2025-06-02 23:53:56 +0300

    Add files via upload

---
+++ app/issuing-a-visa/page.js
@@ -0,0 +1,37 @@
+"use client"
+import SecondNavHeader from "@/components/common/TopBar";
+import NavHeader2 from "@/components/common/Navbar";
+import PageHeader from "@/components/common/PageHeader"
+import Visa from "@/components/service-hub/Visa"
+import FeedBackSection from "@/components/contact/FeedBackSection"
+import ContactUsSection from "@/components/contact/ContactUsSection"
+import Footer from "@/components/common/Footer"
+
+export default function Page() {
+  return (
+    <div>
+    <SecondNavHeader />
+     <NavHeader2 />
+     <PageHeader
+  breadcrumbs={[
+    { label: "Home" },
+    { label: "FAQs", href: "/" },
+    { label: "Issuing a visa" }, 
+  ]}
+  title="Issuing a visa"
+  description="Find answers to common questions about our products, services, and policies."
+/>
+
+      <Visa />
+       <ContactUsSection />
+      <FeedBackSection />
+      
+      <Footer />
+    </div>
+  )
+}
+
+
+
+
+
