3c9a56b144e8e18b5fd3cb9f18ec7ed2e82e22cc
Haifaa-t <h.aloraini-t@tamkeentech.sa>
2025-06-03 16:08:45 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,14 @@
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        ibm: ['"IBM Plex Sans Arabic"', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
