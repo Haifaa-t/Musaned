6ec468a991877c021e3c09c0866b3911edbc0c4a
Haifaa-t <h.aloraini-t@tamkeentech.sa>
2025-06-03 15:59:42 +0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,33 +1,35 @@
 "use client";
+
 import SecondNavHeader from "@/components/common/TopBar";
 import NavHeader2 from "@/components/common/Navbar";
-import PageHeader from "@/components/common/PageHeader"
+import PageHeader from "@/components/common/PageHeader";
 import ServiceHubSection from "@/components/service-hub/ServiceHub";
 import ContactUsSection from "@/components/contact/ContactUsSection";
-
 import FeedbackSection from "@/components/contact/FeedBackSection";
 import Footer from "@/components/common/Footer";
+
 export default function FAQPage() {
   return (
-    <main className="w-full">
+    <main className="w-full font-ibm">
       <SecondNavHeader />
       <NavHeader2 />
-        <PageHeader
-     breadcrumbs={[
-       { label: "Home" },
-       { label: "FAQs", href: "/" },
-       
-     ]}
-     title="FAQs"
-     description="Find answers to common questions about our products, services, and policies."
-   />
 
-      <ServiceHubSection/>
+      <PageHeader
+        breadcrumbs={[
+          { label: "Home" },
+          { label: "FAQs", href: "/" },
+        ]}
+        title="FAQs"
+        description="Find answers to common questions about our products, services, and policies."
+      />
+
+      <ServiceHubSection />
       <ContactUsSection />
       <FeedbackSection />
-      <Footer /> 
+      <Footer />
     </main>
   );
 }
+
 
 
