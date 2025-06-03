8253402ca3e7dc73c6bf26161820e49dc65afdcb
Haifaa-t <h.aloraini-t@tamkeentech.sa>
2025-06-03 15:58:16 +0300

    Update page.js

--- app/issuing-a-visa/page.js
+++ app/issuing-a-visa/page.js
@@ -1,35 +1,35 @@
-"use client"
+"use client";
+
 import SecondNavHeader from "@/components/common/TopBar";
 import NavHeader2 from "@/components/common/Navbar";
-import PageHeader from "@/components/common/PageHeader"
-import Visa from "@/components/service-hub/Visa"
-import FeedBackSection from "@/components/contact/FeedBackSection"
-import ContactUsSection from "@/components/contact/ContactUsSection"
-import Footer from "@/components/common/Footer"
+import PageHeader from "@/components/common/PageHeader";
+import Visa from "@/components/service-hub/Visa";
+import FeedBackSection from "@/components/contact/FeedBackSection";
+import ContactUsSection from "@/components/contact/ContactUsSection";
+import Footer from "@/components/common/Footer";
 
 export default function Page() {
   return (
-    <div>
-    <SecondNavHeader />
-     <NavHeader2 />
-     <PageHeader
-  breadcrumbs={[
-    { label: "Home" },
-    { label: "FAQs", href: "/" },
-    { label: "Issuing a visa" }, 
-  ]}
-  title="Issuing a visa"
-  description="Find answers to common questions about our products, services, and policies."
-/>
-
+    <div className="font-ibm">
+      <SecondNavHeader />
+      <NavHeader2 />
+      <PageHeader
+        breadcrumbs={[
+          { label: "Home" },
+          { label: "FAQs", href: "/" },
+          { label: "Issuing a visa" },
+        ]}
+        title="Issuing a visa"
+        description="Find answers to common questions about our products, services, and policies."
+      />
       <Visa />
-       <ContactUsSection />
+      <ContactUsSection />
       <FeedBackSection />
-      
       <Footer />
     </div>
-  )
+  );
 }
+
 
 
 
