5fc732b7428f3bc89e855f4e8cf530b376bbe7fa
Haifaa-t <h.aloraini-t@tamkeentech.sa>
2025-06-03 15:55:16 +0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,20 @@
-// app/layout.js
 import "./globals.css";
 
 export const metadata = {
-  title: "Musaned",
-  description: "Government Services Platform",
+  title: "Musaned Platform",
+  description: "Official Musaned website",
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="ar">
+      <head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Arabic:wght@400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+      </head>
+      <body className="font-ibm">{children}</body>
     </html>
   );
 }
